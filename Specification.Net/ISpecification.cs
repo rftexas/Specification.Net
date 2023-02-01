4fe2bb72a73abb17f18a9eed567da939f7f7bc6a
Ryan Freels <ryan@freelstechnology.com>
2023-01-31 19:52:22 -0600

    Allow dotnet 6 and 7 to be targetted

--- Specification.Net/ISpecification.cs
+++ Specification.Net/ISpecification.cs
@@ -1,10 +1,13 @@
 using System.Linq.Expressions;
 using LinqExpression = System.Linq.Expressions.Expression;
 
-namespace Specification.Net;
+namespace Specification.Net
+{
 
-public interface ISpecification<T>{
-    bool IsSatisfied(T item);
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T item);
 
-    Expression<Func<T, bool>> Expression { get; }
+        Expression<Func<T, bool>> Expression { get; }
+    }
 }
